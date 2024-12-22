0a8c9c6263f7bfbf81fed905aaafbb006b511861
AlfredoPinto20 <agpintoalastre2@gmail.com>
2024-12-22 12:55:57 +0100

    Update Principal.java
    Añadida una despedida

--- Proyecto_GitHub/src/Principal.java
+++ Proyecto_GitHub/src/Principal.java
@@ -4,5 +4,6 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Holi Wan Kenobi");
 		System.out.println("Hola mundo");
+		System.out.println("Adios a todos");
 	}
 }
